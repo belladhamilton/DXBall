66bd3fa04afb21976d0f86e186722113381475c2
belladhamilton <ihamilton@rollins.edu>
2025-09-08 11:49:41 -0400

    DXBall Update 9/8

--- DXBall/Assets/Scripts/paddlecontroller.cs
+++ DXBall/Assets/Scripts/paddlecontroller.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
-
-public class paddlecontroller : MonoBehaviour
+public class PaddleController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+Rigidbody2D pad;
+Vector2 initial;
+public float displacement;
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+void Start()
+{
+pad = GetComponent<Rigidbody2D>();
+initial = pad.transform.localPosition;
 }
+// Update is called once per frame
+void Update()
+{
+if ((Input.GetKey(KeyCode.RightArrow))){
+if (initial.x<=9.75)
+initial.x=initial.x+displacement;
+}
+else if((Input.GetKey(KeyCode.LeftArrow))){
+if (initial.x>-9.75)
+initial.x=initial.x-displacement;
+}
+pad.MovePosition(initial);
+}
+}
+
